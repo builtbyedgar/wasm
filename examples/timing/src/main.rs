ef13e94797e2f753afef899580b0544a3773b7be
builtbyedgar <eledgaar@gmail.com>
2023-09-03 20:44:17 +0200

    refactor: closing control

--- examples/timing/src/main.rs
+++ examples/timing/src/main.rs
@@ -1,30 +1,40 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{ Arc, Mutex };
 use std::thread;
 use std::time::{ Duration, Instant };
 
 struct Clock<F> {
     interval: Duration,
     // callback: F,
-    // Envoltura en Arc<Mutex> para compartirlo de manera segura entre hilos y permitir 
+    // Envoltura en Arc<Mutex> para compartirlo de manera segura entre hilos y permitir
     // que se llame desde el bucle sin preocuparte por la movilidad.
     callback: Arc<Mutex<F>>,
+    should_stop: Arc<Mutex<bool>>,
 }
 
 // FnMut
 impl<F> Clock<F> where F: FnMut() + Send + Sync + 'static {
     fn new(interval: Duration, callback: F) -> Self {
-        Clock { interval, callback: Arc::new(Mutex::new(callback)) }
+        Clock {
+            interval,
+            callback: Arc::new(Mutex::new(callback)),
+            should_stop: Arc::new(Mutex::new(false)),
+        }
     }
 
     // Inicia un hilo que ejecuta el bucle principal.
     fn start(self) -> Result<(), Box<dyn std::error::Error>> {
         let callback = self.callback.clone();
+        let should_stop = self.should_stop.clone();
 
         let handle = thread::spawn(move || {
             let mut expected = Instant::now() + self.interval;
 
-            
             loop {
+                // Verifica si se debe detener el bucle.
+                if *should_stop.lock().unwrap() {
+                    break;
+                }
+
                 let drift = Instant::now() - expected;
                 expected += self.interval;
 
@@ -38,8 +48,18 @@ impl<F> Clock<F> where F: FnMut() + Send + Sync + 'static {
             }
         });
 
-        handle.join()?;
-        Ok(())
+        match handle.join() {
+            Ok(_) => Ok(()),
+            Err(e) => Err(format!("Thread join error: {:?}", e).into()),
+        }
+    }
+
+    fn stop(&self) {
+        *self.should_stop.lock().unwrap() = true;
+    }
+
+    fn change_interval(&mut self, new_interval: Duration) {
+        self.interval = new_interval;
     }
 }
 
