593cb94d81a6e5db2aafe8285f32550ebac4561f
builtbyedgar <eledgaar@gmail.com>
2023-09-03 20:33:16 +0200

    refactor: new Clock

--- examples/timing/src/main.rs
+++ examples/timing/src/main.rs
@@ -1,75 +1,52 @@
-use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{ Duration, Instant };
 
-mod sequencer;
-
-// Opciones del secuenciador
-struct Options {
-    steps: u8,
-    tempo: u8,
+struct Clock<F> {
+    interval: Duration,
+    // callback: F,
+    // Envoltura en Arc<Mutex> para compartirlo de manera segura entre hilos y permitir 
+    // que se llame desde el bucle sin preocuparte por la movilidad.
+    callback: Arc<Mutex<F>>,
 }
 
-// Definimos un evento custom
-struct Event {
-    // Tiempo en milisegundos
-    t: u128,
-}
+// FnMut
+impl<F> Clock<F> where F: FnMut() + Send + Sync + 'static {
+    fn new(interval: Duration, callback: F) -> Self {
+        Clock { interval, callback: Arc::new(Mutex::new(callback)) }
+    }
 
-// Main function
-fn main() {
-    // Crear un canal de comunicación entre hilos
-    // (multiple-producer, single-consumer).
-    // tx es el extremo de envío (sender)
-    // rx es el extremo de recepción (receiver).
-    // tx se utiliza para enviar datos al canal, y rx se utiliza para recibir.
-    let (tx, rx) = mpsc::channel();
+    // Inicia un hilo que ejecuta el bucle principal.
+    fn start(self) {
+        let callback = self.callback.clone();
 
-    // Esta marcianada es una práctica bastante común en Rust y se utiliza para limitar el
-    // alcance de la variable `tx` y controlorar su duración de vida. Esto garantiza que
-    // los recursos sean liberados de forma adecuada y no se retengan mas tiempo del necesario.
-    {
-        // Crea un clon del extremo de envío.
-        // Util para tener múltiples propietarios que puedan enviar datos al canal.
-        // Al clonarlo puedo pasarlo a diferentes hilos o partes del programa, y cada uno
-        // podrá enviar datos al mismo canal de forma independiente.
-        let tx_clone = mpsc::Sender::clone(&tx);
+        let _ = thread::spawn(move || {
+            let mut expected = Instant::now() + self.interval;
 
-        let mut seq = sequencer::new(120.0);
-        // Crea un nuevo subproceso y comienza a ejecutar el código dentro del bloque de cierre.
-        // El subproceso se inicia con un nuevo hilo y se le asigna a la variable sub.
-        let sub = thread::spawn(move || {
-            let mut tick = Instant::now();
-
+            
             loop {
-                // spera hasta que el tiempo actual sea mayor que el valor en tick.
-                while Instant::now() < tick {
-                    // Mientras tanto, se usa thread::yield_now() para permitir
-                    // que otros hilos se ejecuten.
-                    thread::yield_now();
-                }
+                let drift = Instant::now() - expected;
+                expected += self.interval;
 
-                // Enviar evento al canal de comunicación entre subprocesos.
-                let _ = tx.send(Event {
-                    t: (Instant::now() - tick).as_micros(),
-                });
+                // Bloquear el mutex para acceder a la función de devolución de llamada.
+                let mut callback = callback.lock().unwrap();
+                callback();
 
-                let accuracy = Duration::from_millis(1);
-                // Incrementar en la instancia e igualarlo
-                seq.tick();
-                tick += seq.tick;
-
-                // Establece el subproceso en un estado de espera.
-                thread::park_timeout(tick - accuracy - Instant::now());
+                thread::park_timeout(
+                    Duration::from_millis((self.interval - drift).as_millis() as u64)
+                );
             }
         });
-
-
-
-        // TODO: dev only
-        // Espera durante un tiempo para que el subproceso tenga tiempo de ejecutarse.
-        thread::sleep(Duration::from_secs(3));
-        // Asegúra que el subproceso termine antes de salir.
-        let _ = sub.join();
     }
+}
+
+fn main() {
+    fn callback_fn() {
+        println!("Callback function called");
+    }
+
+    let t = Clock::new(Duration::from_secs(1), callback_fn);
+    t.start();
+
+    thread::sleep(Duration::from_secs(10));
 }
